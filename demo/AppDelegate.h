be4ff6ddee6bfa0171d1028766b04025ee5094c9
Ashwani Sharma <human@human.local>
2015-10-21 16:16:30 +0530

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Ashwani Sharma on 21/10/15.
+//  Copyright © 2015 Ashwani Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by Ashwani Sharma on 21/10/15.
+//  Copyright © 2015 Ashwani Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
